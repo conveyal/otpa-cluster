b9f18e49db70d7546e6bfbbd4b7719ae7ecc511e
Brandon Martin-Anderson <badhill@gmail.com>
2014-06-17 18:32:39 -0700

    remove cruft

--- src/main/java/com/conveyal/akkaplay/actors/PrimeTester.java
+++ src/main/java/com/conveyal/akkaplay/actors/PrimeTester.java
@@ -1,30 +1,20 @@
 package com.conveyal.akkaplay.actors;
 
-import java.util.Random;
-
 import com.conveyal.akkaplay.CosmicRayException;
 import com.conveyal.akkaplay.message.*;
 
-import akka.actor.ActorRef;
 import akka.actor.UntypedActor;
-import akka.dispatch.BoundedMessageQueueSemantics;
-import akka.dispatch.RequiresMessageQueue;
 
 public class PrimeTester extends UntypedActor {
 	
-	private Random rr;
 
 	PrimeTester(){
-		rr = new Random();
 	}
 
   @Override
   public void onReceive(Object message) throws CosmicRayException {
 	  if( message instanceof PrimeCandidate ){
-//		  if(rr.nextDouble()<0.01){
-//			  throw new CosmicRayException();
-//		  }
-		  
+		  		  		  
 		  PrimeCandidate pc = (PrimeCandidate)message;
 		  boolean ret = isPrime(pc.num);
 		  getSender().tell(new WorkResult(pc.jobId,pc.num,ret), getSelf());
