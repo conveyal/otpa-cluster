76d364d838deeca17f9a577fbdd13c956154d9d8
Kevin Webb <kpwebb@gmail.com>
2014-07-10 23:17:46 -0400

    added offline and json support for pointset

--- src/main/java/com/conveyal/otpac/S3Datastore.java
+++ src/main/java/com/conveyal/otpac/S3Datastore.java
@@ -10,6 +10,7 @@ import java.util.Enumeration;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
 import org.opentripplanner.analyst.PointSet;
 
@@ -17,41 +18,81 @@ import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.profile.ProfileCredentialsProvider;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectResult;
 import com.amazonaws.services.s3.model.S3Object;
 import com.google.common.io.Files;
 
 public class S3Datastore {
 	
+	private static String CACHE_DIR = "cache/";
+	
 	private AmazonS3Client s3;
-
-	public S3Datastore(){
-		// grab credentials from "~.aws/credentials"
-		AWSCredentials creds = new ProfileCredentialsProvider().getCredentials();
-		s3 = new AmazonS3Client(creds);
+	private Boolean workOffline = false;
+	
+	public S3Datastore() {
+		// don't work offline by default
+		this(false);
+	}
+	
+	public S3Datastore(Boolean workOffline){
+		
+		// allow the data store to work offline with cached data and skip S3 connection
+		this.workOffline = workOffline;
+		
+		if(!this.workOffline) {
+			// grab credentials from "~.aws/credentials"
+			AWSCredentials creds = new ProfileCredentialsProvider().getCredentials();
+			s3 = new AmazonS3Client(creds);
+		}
+	}
+	
+	
+	// adds file to S3 Data store or offline cache (if working offline)
+	public void addPointSet(File pointSetFile) throws IOException {
+		
+		String ptsLoc = pointSetFile.getName();
+		
+		if(!this.workOffline) {
+			PutObjectResult obj = s3.putObject("pointsets",ptsLoc, pointSetFile);	
+		} 
+		else {
+			FileUtils.moveFileToDirectory(pointSetFile, new File(CACHE_DIR), true);
+		}
 	}
 
 	public PointSet getPointset(String ptsLoc) throws Exception {
 
-		// get pointset metadata from S3
-		S3Object obj = s3.getObject("pointsets",ptsLoc);
-		ObjectMetadata objMet = obj.getObjectMetadata();
+		String cachePath;
 		
-		// if it's not already cached, do so
-		String objEtag = objMet.getETag();
-		File cachedFile = new File("cache/"+objEtag+"-"+ptsLoc);
-		if(!cachedFile.exists()){
-			Util.saveFile( cachedFile, obj.getObjectContent(), objMet.getContentLength(), true);
+		if(!this.workOffline) {
+			// get pointset metadata from S3
+			S3Object obj = s3.getObject("pointsets",ptsLoc);
+			ObjectMetadata objMet = obj.getObjectMetadata();
+			
+			// if it's not already cached, do so
+			String objEtag = objMet.getETag();
+			
+			cachePath = CACHE_DIR + objEtag+"-"+ptsLoc;
+			
+			File cachedFile = new File(cachePath);
+			if(!cachedFile.exists()){
+				Util.saveFile( cachedFile, obj.getObjectContent(), objMet.getContentLength(), true);
+			}
 		}
+		else 
+			cachePath = CACHE_DIR + ptsLoc;
+		
+		
 		
 		// grab it from the cache
-		InputStream objectData = new FileInputStream( cachedFile );
+		InputStream objectData = new FileInputStream(cachePath);
 		
 		PointSet ret=null;
 		if( isCsv(ptsLoc) ){
-			ret = PointSet.fromCsv( "cache/"+objEtag+"-"+ptsLoc );
+			ret = PointSet.fromCsv(cachePath);
 		} else if( isZippedShapefile(ptsLoc) ){
 			File tempDir = Files.createTempDir();
-			String shapefileName = unzipShapefile( tempDir, "cache/"+objEtag+"-"+ptsLoc );
+			String shapefileName = unzipShapefile( tempDir,cachePath);
 			if(shapefileName == null){
 				objectData.close();
 				throw new Exception( "Zip does no contain a shapefile" );
@@ -59,6 +100,8 @@ public class S3Datastore {
 			
 			ret = PointSet.fromShapefile( tempDir.getPath()+"/"+shapefileName);
 			tempDir.delete();
+		} else if(isJson(ptsLoc)) {
+			ret = PointSet.fromGeoJson(cachePath);
 		}
 		
 		objectData.close();
@@ -71,6 +114,12 @@ public class S3Datastore {
 		return parts[parts.length-2].equals("shp") && parts[parts.length-1].equals("zip");
 	}
 
+	private boolean isJson(String ptsLoc) {
+		String[] parts = ptsLoc.split("\\.");
+		String formatPart = parts[parts.length-1];
+		return formatPart.equals("json");
+	}
+	
 	private boolean isCsv(String ptsLoc) {
 		String[] parts = ptsLoc.split("\\.");
 		String formatPart = parts[parts.length-1];
